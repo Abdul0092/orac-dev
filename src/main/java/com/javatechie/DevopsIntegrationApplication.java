f032c297f74305dbd18a38b4db1ec580188ab4ad
abdul <abdul.bari101001.com>
2023-03-09 13:10:58 +0530

    first commit

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to ORACLE INDIA PVT LTD";
 	}
 
 	public static void main(String[] args) {
